a139429271b18b1851440c524601dd179423b83d
João Henrique <jhenrique.caval@hotmail.com>
2026-03-16 01:29:27 -0300

    feat: ajuste na hero, imagem nova

--- src/hero.jsx
+++ src/hero.jsx
@@ -82,7 +82,7 @@ function Hero() {
           initial="hidden"
           animate="show"
         >
-          <img src="joao-pic.png" alt="" className="hero-img" />
+          <img src="joao-pic2.png" alt="" className="hero-img" />
         </motion.div>
       </div>
 
